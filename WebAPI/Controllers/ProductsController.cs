c5e17ada5395792b2e792a6a793fc4f71376cd1a
Selçuk Yarıcı <selcukyarici@hotmail.com>
2021-03-13 23:56:07 +0300

    13 Mart 2021 18. gün ders sonu

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -31,7 +31,7 @@ namespace WebAPI.Controllers
         {
             //Swagger
             //Dependency chain--
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             var result = _productService.GetAll();
             if (result.Success)
@@ -56,6 +56,17 @@ namespace WebAPI.Controllers
         public IActionResult Add(Product product)
         {
             var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
             if (result.Success)
             {
                 return Ok(result);
